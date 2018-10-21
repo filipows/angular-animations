987bf42659a86e2007959c11278e262f78aa8f79
Chris Filipowski <kfilipowski+git@gmail.com>
2018-10-21 19:26:55 +0800

    feat(demo-app): add material components

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatSelectModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AngularImgComponent } from './angular-img/angular-img.component';
@@ -11,7 +12,16 @@ import { DemoMainComponent } from './demo-main/demo-main.component';
 
 @NgModule({
   declarations: [AppComponent, AngularImgComponent, DemoOnEnterOnLeaveComponent, DemoMainComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
