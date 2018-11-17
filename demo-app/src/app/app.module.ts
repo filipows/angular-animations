d4a67e9d183e54e2fcea0c64a7f0f5ea51b62b1a
Chris Filipowski <kfilipowski+git@gmail.com>
2018-11-17 23:19:52 +0800

    feat(): add experiments link to nav

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -2,7 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule, MatSelectModule, MatButtonModule, MatToolbarModule, MatInputModule } from '@angular/material';
+import {
+  MatFormFieldModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AngularImgComponent } from './angular-img/angular-img.component';
@@ -30,7 +39,10 @@ import { DemoDynamicParamsComponent } from './demo-dynamic-params/demo-dynamic-p
     MatSelectModule,
     MatButtonModule,
     MatToolbarModule,
-    MatInputModule
+    MatInputModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
