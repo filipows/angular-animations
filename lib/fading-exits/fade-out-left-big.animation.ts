d6ff72891f811c02e7909eb6f523755149a3ef8b
Chris Filipowski <3.3fullpower@gmail.com>
2020-10-25 09:40:11 +0800

    feat(easing-exits): add easing options for fading exits

--- lib/fading-exits/fade-out-left-big.animation.ts
+++ lib/fading-exits/fade-out-left-big.animation.ts
@@ -1,7 +1,10 @@
 import { animate, animation, AnimationTriggerMetadata, keyframes, style, transition, trigger } from '@angular/animations';
 
-import { IAnimationOptions } from '../common/interfaces';
+import { IAnimationOptions, Easing } from '../common/interfaces';
 import { useAnimationIncludingChildren } from '../common/use-animation-including-children';
+
+const DEFAULT_DURATION = 1000;
+const DEFAULT_EASING: Easing = 'ease';
 
 export interface IFadeOutLeftBigAnimationOptions extends IAnimationOptions {
   /**
@@ -10,24 +13,29 @@ export interface IFadeOutLeftBigAnimationOptions extends IAnimationOptions {
    * Default: 2000px
    */
   translate?: string;
+
+  /**
+   * Easing
+   *
+   * Default: 'ease'
+   */
+  easing?: Easing;
 }
 
-const fadeOutLeftBig = () =>
+const fadeOutLeftBig = (easing: Easing = DEFAULT_EASING) =>
   animation([
     animate(
       '{{duration}}ms {{delay}}ms',
       keyframes([
-        style({ opacity: 1, transform: 'translate3d(0, 0, 0)', easing: 'ease', offset: 0 }),
-        style({ opacity: 0, transform: 'translate3d(-{{translate}}, 0, 0)', easing: 'ease', offset: 1 })
+        style({ opacity: 1, transform: 'translate3d(0, 0, 0)', easing, offset: 0 }),
+        style({ opacity: 0, transform: 'translate3d(-{{translate}}, 0, 0)', easing, offset: 1 })
       ])
     )
   ]);
 
-const DEFAULT_DURATION = 1000;
-
 export function fadeOutLeftBigAnimation(options?: IFadeOutLeftBigAnimationOptions): AnimationTriggerMetadata {
   return trigger((options && options.anchor) || 'fadeOutLeftBig', [
-    transition('0 => 1', [...useAnimationIncludingChildren(fadeOutLeftBig(), options)], {
+    transition('0 => 1', [...useAnimationIncludingChildren(fadeOutLeftBig(options?.easing), options)], {
       params: {
         delay: (options && options.delay) || 0,
         duration: (options && options.duration) || DEFAULT_DURATION,
@@ -39,7 +47,7 @@ export function fadeOutLeftBigAnimation(options?: IFadeOutLeftBigAnimationOption
 
 export function fadeOutLeftBigOnLeaveAnimation(options?: IFadeOutLeftBigAnimationOptions): AnimationTriggerMetadata {
   return trigger((options && options.anchor) || 'fadeOutLeftBigOnLeave', [
-    transition(':leave', [...useAnimationIncludingChildren(fadeOutLeftBig(), options)], {
+    transition(':leave', [...useAnimationIncludingChildren(fadeOutLeftBig(options?.easing), options)], {
       params: {
         delay: (options && options.delay) || 0,
         duration: (options && options.duration) || DEFAULT_DURATION,
