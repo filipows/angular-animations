ef3e36af66cb7e8a970538dbb53549dc0d4a0616
Chris Filipowski <3.3fullpower@gmail.com>
2020-10-25 10:56:58 +0800

    feat(sliding-exits): add easing option for sliding exits

--- lib/sliding-exits/slide-out-down.animation.ts
+++ lib/sliding-exits/slide-out-down.animation.ts
@@ -1,7 +1,10 @@
 import { animate, animation, AnimationTriggerMetadata, keyframes, style, transition, trigger } from '@angular/animations';
 
-import { IAnimationOptions } from '../common/interfaces';
+import { IAnimationOptions, Easing } from '../common/interfaces';
 import { useAnimationIncludingChildren } from '../common/use-animation-including-children';
+
+const DEFAULT_DURATION = 1000;
+const DEFAULT_EASING: Easing = 'ease';
 
 export interface ISlideOutDownAnimationOptions extends IAnimationOptions {
   /**
@@ -10,24 +13,29 @@ export interface ISlideOutDownAnimationOptions extends IAnimationOptions {
    * Default: 100%
    */
   translate?: string;
+
+  /**
+   * Easing
+   *
+   * Default: 'ease'
+   */
+  easing?: Easing;
 }
 
-const slideOutDown = () =>
+const slideOutDown = (easing: Easing = DEFAULT_EASING) =>
   animation([
     animate(
       '{{duration}}ms {{delay}}ms',
       keyframes([
-        style({ transform: 'translate3d(0, 0, 0)', easing: 'ease', offset: 0 }),
-        style({ transform: 'translate3d(0, {{translate}}, 0)', visibility: 'hidden', easing: 'ease', offset: 1 })
+        style({ transform: 'translate3d(0, 0, 0)', easing, offset: 0 }),
+        style({ transform: 'translate3d(0, {{translate}}, 0)', visibility: 'hidden', easing, offset: 1 })
       ])
     )
   ]);
 
-const DEFAULT_DURATION = 1000;
-
 export function slideOutDownAnimation(options?: ISlideOutDownAnimationOptions): AnimationTriggerMetadata {
   return trigger((options && options.anchor) || 'slideOutDown', [
-    transition('0 => 1', [...useAnimationIncludingChildren(slideOutDown(), options)], {
+    transition('0 => 1', [...useAnimationIncludingChildren(slideOutDown(options?.easing), options)], {
       params: {
         delay: (options && options.delay) || 0,
         duration: (options && options.duration) || DEFAULT_DURATION,
@@ -39,7 +47,7 @@ export function slideOutDownAnimation(options?: ISlideOutDownAnimationOptions): 
 
 export function slideOutDownOnLeaveAnimation(options?: ISlideOutDownAnimationOptions): AnimationTriggerMetadata {
   return trigger((options && options.anchor) || 'slideOutDownOnLeave', [
-    transition(':leave', [...useAnimationIncludingChildren(slideOutDown(), options)], {
+    transition(':leave', [...useAnimationIncludingChildren(slideOutDown(options?.easing), options)], {
       params: {
         delay: (options && options.delay) || 0,
         duration: (options && options.duration) || DEFAULT_DURATION,
--- lib/sliding-exits/slide-out-left.animation.ts
+++ lib/sliding-exits/slide-out-left.animation.ts
@@ -1,7 +1,10 @@
 import { animate, animation, AnimationTriggerMetadata, keyframes, style, transition, trigger } from '@angular/animations';
 
-import { IAnimationOptions } from '../common/interfaces';
+import { IAnimationOptions, Easing } from '../common/interfaces';
 import { useAnimationIncludingChildren } from '../common/use-animation-including-children';
+
+const DEFAULT_DURATION = 1000;
+const DEFAULT_EASING: Easing = 'ease';
 
 export interface ISlideOutLeftAnimationOptions extends IAnimationOptions {
   /**
@@ -10,24 +13,29 @@ export interface ISlideOutLeftAnimationOptions extends IAnimationOptions {
    * Default: 100%
    */
   translate?: string;
+
+  /**
+   * Easing
+   *
+   * Default: 'ease'
+   */
+  easing?: Easing;
 }
 
-const slideOutLeft = () =>
+const slideOutLeft = (easing: Easing = DEFAULT_EASING) =>
   animation([
     animate(
       '{{duration}}ms {{delay}}ms',
       keyframes([
-        style({ transform: 'translate3d(0, 0, 0)', easing: 'ease', offset: 0 }),
-        style({ transform: 'translate3d(-{{translate}}, 0, 0)', visibility: 'hidden', easing: 'ease', offset: 1 })
+        style({ transform: 'translate3d(0, 0, 0)', easing, offset: 0 }),
+        style({ transform: 'translate3d(-{{translate}}, 0, 0)', visibility: 'hidden', easing, offset: 1 })
       ])
     )
   ]);
 
-const DEFAULT_DURATION = 1000;
-
 export function slideOutLeftAnimation(options?: ISlideOutLeftAnimationOptions): AnimationTriggerMetadata {
   return trigger((options && options.anchor) || 'slideOutLeft', [
-    transition('0 => 1', [...useAnimationIncludingChildren(slideOutLeft(), options)], {
+    transition('0 => 1', [...useAnimationIncludingChildren(slideOutLeft(options?.easing), options)], {
       params: {
         delay: (options && options.delay) || 0,
         duration: (options && options.duration) || DEFAULT_DURATION,
@@ -39,7 +47,7 @@ export function slideOutLeftAnimation(options?: ISlideOutLeftAnimationOptions): 
 
 export function slideOutLeftOnLeaveAnimation(options?: ISlideOutLeftAnimationOptions): AnimationTriggerMetadata {
   return trigger((options && options.anchor) || 'slideOutLeftOnLeave', [
-    transition(':leave', [...useAnimationIncludingChildren(slideOutLeft(), options)], {
+    transition(':leave', [...useAnimationIncludingChildren(slideOutLeft(options?.easing), options)], {
       params: {
         delay: (options && options.delay) || 0,
         duration: (options && options.duration) || DEFAULT_DURATION,
